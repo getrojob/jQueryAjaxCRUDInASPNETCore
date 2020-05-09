5aa643878fa5e77f4b60b94b1473d4212b32121f
Getulio Rodrigues <getrojob@gmail.com>
2020-05-09 14:56:03 -0300

    Add project files.

---
+++ jQueryAjaxCRUDInASPNETCore/Migrations/20200509020919_AddedDataColumn2.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace jQueryAjaxCRUDInASPNETCore.Migrations
+{
+    public partial class AddedDataColumn2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SWIFTCode",
+                table: "Transactions",
+                type: "nvarchar(11)",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BeneficiaryName",
+                table: "Transactions",
+                type: "nvarchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Amount",
+                table: "Transactions",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(11)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountNumber",
+                table: "Transactions",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BankName",
+                table: "Transactions",
+                type: "nvarchar(100)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BankName",
+                table: "Transactions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SWIFTCode",
+                table: "Transactions",
+                type: "nvarchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(11)",
+                oldMaxLength: 11);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BeneficiaryName",
+                table: "Transactions",
+                type: "nvarchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Amount",
+                table: "Transactions",
+                type: "nvarchar(11)",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountNumber",
+                table: "Transactions",
+                type: "nvarchar(12)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldMaxLength: 12);
+        }
+    }
+}
---
+++ jQueryAjaxCRUDInASPNETCore/Models/TransactionDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace jQueryAjaxCRUDInASPNETCore.Models
+{
+    public class TransactionDbContext : DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
+        { }
+        public DbSet<TransactionModel> Transactions { get; set; }
+    }
+}
